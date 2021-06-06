cbbb453ff171dfd88452bdbb90804a16ceb037df
bdoor <bdoor92@gmail.com>
2021-06-06 08:31:08 +0300

    add col

---
+++ js.js
@@ -0,0 +1,15 @@
+console.log("hello");
+let neww = "mona";
+console.log(typeof neww);
+let colors = ["red","blue","green"]
+let ind = colors
+ind[1] = "yellow"
+console.log(ind)
+ind.push("non")
+console.log(ind)
+ind.unshift("new")
+console.log(ind)
+colors.pop()
+console.log(ind)
+colors.shift()
+console.log(ind)
